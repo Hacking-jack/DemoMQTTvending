b0421a17bf0877b6290eaa78813e0e8ee1708c3d
hacking-jack <daniel.rodriguezti1@gmail.com>
2023-12-22 20:21:25 +0100

    test

--- mqtt/mqttCon.py
+++ mqtt/mqttCon.py
@@ -1,5 +1,21 @@
 import paho.mqtt.client as mqtt
-from gpio.gpioController import *
+import RPi.GPIO as GPIO
+import time
+
+
+def activa(msg: str):
+    # Set the GPIO mode to BCM
+    GPIO.setmode(GPIO.BCM)
+
+    # Set up GPIO pin 17 as an output
+    GPIO.setup(21, GPIO.OUT)
+
+    if (msg == "ON"):
+        GPIO.output(21, GPIO.HIGH)
+        time.sleep(1500)
+        GPIO.output(21, GPIO.LOW)
+        time.sleep(1)
+
 
 class MQTTReader:
     def __init__(self, broker_host, broker_port, topic,username=None, password=None):
